e8f1415a63233a42eb951796405932e0a7e713fc
nmsriharsha2004@gmail.com <nmsriharsha2004@gmail.com>
2024-02-20 17:37:48 +0530

    clearing some file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import {BrowserRouter , Route , Switch} from 'react-router-dom';
 
-// import Navbar from './Components/Navbar';
-import About from './Components/About';
 import SignIn from './Components/SignIn';
 import SignUp from './Components/Signup';
 import Home from './Components/Home';
@@ -17,7 +15,6 @@ function App() {
         <div className='content'>
           <Switch>
               <Route path="/" exact> <Home/> </Route>
-              <Route path="/about"> <About/> </Route>
               <Route path="/SignUp" exact> <SignUp/> </Route>
               <Route path="/SignIn" exact> <SignIn/> </Route>
               <Route path="/NotesList"><NotesList/></Route>
